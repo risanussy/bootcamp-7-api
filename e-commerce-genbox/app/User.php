da5e8d8c176a7bc43715ed5a433b3dd3e58d575e
Joyo Nurdiansyah <61000472+joyonurdiansyah@users.noreply.github.com>
2024-02-16 09:24:56 +0700

    rewrite fillable in User Model

--- e-commerce-genbox/app/User.php
+++ e-commerce-genbox/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password', 'confirm_password'
     ];
 
     /**
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profilToko()
+    {
+        return $this->hasMany(ProfilToko::class);
+    }
 }
